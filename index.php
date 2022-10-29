74b4af185527a23c7ce8bcd170bc1ab6b53791aa
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-29 18:36:04 +0200

    add actors

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+require_once __DIR__ . '/movie.php';
 
+$movie = new Movie ('Batman Begins', 'Christopher Nolan', '', '2005', '140 minutes', '', '', '');
+// add actors array
+$movie->addActor('Christian Bale');
+$movie->addActor('Michael Caine');
+$movie->addActor('Liam Neeson');
+$movie->addActor('Katie Holmes');
+$movie->addActor('Gary Oldman');
+$movie->addActor('Cillian Murphy');
+$movie->addActor('Morgan Freeman');
 
 ?>
