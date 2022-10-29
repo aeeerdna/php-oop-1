ac417e1d2376434ff60b8e8c84bf69df95690591
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-29 18:07:07 +0200

    movie properties

--- movie.php
+++ movie.php
@@ -7,5 +7,21 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
    => all'interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
  -->
+<?php
+ class Movie {
+    // movie properties
+    public $title;
+    public $director;
+    public $cast = [];
+    public $production_company;
 
- 
+    public $release_date;
+    public $running_time;
+    public $language;
+    public $genre;
+    public $plot;
+
+
+ }
+
+ ?>
